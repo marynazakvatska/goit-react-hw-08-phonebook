346f32c4794c03c90031b207403d57ed7edba519
marynazakvatska <maryna.zakvatska@gmail.com>
2021-08-31 00:44:43 +0200

    Updates

---
+++ static/js/6.813468a0.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-08-phonebook"]=this["webpackJsonpgoit-react-hw-08-phonebook"]||[]).push([[6],{104:function(e,t,o){"use strict";o.r(t);o(0);var n=o(87),i=o.n(n),c=o(1);t.default=function(){return Object(c.jsx)("div",{className:i.a.container,children:Object(c.jsx)("h1",{className:i.a.title,children:"Welcome to your Phonebook"})})}},87:function(e,t,o){e.exports={container:"HomeViews_container__2MtGr",title:"HomeViews_title__1pW6b"}}}]);
+//# sourceMappingURL=6.813468a0.chunk.js.map
